5372d40498e06b31ad931004ac6179533d87fc59
Ryo Nakamura <upa@haeena.net>
2019-08-16 23:03:12 +0900

    add bw2 random results

--- test-bw2.sh
+++ test-bw2.sh
@@ -23,7 +23,7 @@ function run_bench() {
 	sleep 0.5
 
         cmd="sudo ./bin/benchmark \
-                -d $dir -t $typ -p FIX 0 -n $len -l 30 \
+                -d $dir -t $typ -p RAN 2097152 -n $len -l 30 \
                 -P $mem" > $out
         echo $cmd
         $cmd > $out
@@ -33,6 +33,6 @@ for x in `seq 128`; do
 
 	len=$(( $x * 16 ))
 
-	out="$CRDIR/output/pciebench_bw2_read_ptr-fix_len-${len}.txt"
+	out="$CRDIR/output/pciebench_bw2_read_ptr-random_len-${len}.txt"
 	run_bench $out bw W $len $NETTLP
 done
