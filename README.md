fe0a27a9d832855e2e29ffe2f2c22bb201b56ea7
Ryo Nakamura <upa@haeena.net>
2019-08-16 20:13:11 +0900

    add DMA read latency results

---
+++ README.md
@@ -0,0 +1,4 @@
+
+format of csv files in the output directory is:
+- bandwidth tests: pattern,descriptor,size,bandwidth_gbps
+- latency tests: pattern,descriptor,size,latency_ns
---
+++ test-lat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CRDIR=`pwd`
+PBDIR=~/work/pciebench-netfpga/HOST
+NETTLP=1b:00.0 
+
+function run_bench() {
+
+        out=$1
+	typ=$2
+        dir=$3
+        len=$4
+        mem=$5
+
+	echo
+        echo $len bytes, $mem memory
+	echo output is $out
+
+	cd $PBDIR
+
+        sudo sh ./restart.sh
+        cmd="sudo ./bin/benchmark \
+                -d $dir -t $typ -p FIX 0 -n $len -l 1000 \
+                -P $mem"
+        echo $cmd
+
+        $cmd > $out
+}
+
+
+for len in 8 16 32 64 128 256 512 1024 2048 4096 8192; do
+	out="$CRDIR/output/pciebench_lat_read_ptr-fix_len-${len}.txt"
+	run_bench $out lat W $len $NETTLP
+done
